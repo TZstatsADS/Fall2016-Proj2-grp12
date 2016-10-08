f946ee020ea6227c8096f21932153cb85298f385
Jaime Gacitua <jgacitua@gmail.com>
2016-10-07 21:19:01 -0400

    Restaurant Data Script to call google API
    We need to call the google API from different computers.
    
    Tried Data Science Toolkit as data source, and it is not reliable. Too
    many addresses end with the same lat long.

--- lib/restaurant_data.R
+++ lib/restaurant_data.R
@@ -5,26 +5,73 @@ library(DT)
 library(readr)
 library(data.table)
 library(qmap)
-#setwd("/Users/jgaci/Dropbox/2016_Fall/|") #put your own folder address here
-restaurant.data <- read_csv("../data/DOHMH_New_York_City_Restaurant_Inspection_Results.csv") #load your data set here
-names(restaurant.data) <- make.names(names(restaurant.data))
 
-restaurant.data$full.address <- paste(tolower(restaurant.data$STREET), 
-                                      restaurant.data$BUILDING,", NY, US")
+#
+# Preliminary calculations. Run everything until row 50
+#
+
+preliminary.data.job <- FALSE
+relevant.columns <- c('CAMIS','DBA','CUISINE.DESCRIPTION','full.address')
+
+if(preliminary.data.job){
+  ## ---- Extract Data
+  restaurant.data <- read_csv("../data/DOHMH_New_York_City_Restaurant_Inspection_Results.csv") #load your data set here
+  
+  ## ---- Transform Data
+  names(restaurant.data) <- make.names(names(restaurant.data))
+  
+  restaurant.data$full.address <- paste(tolower(restaurant.data$STREET), 
+                                        restaurant.data$BUILDING,", NY, US")
+  
+  ## ---- Load Data
+  restaurant.data.uniques <- restaurant.data[!duplicated(restaurant.data[,relevant.columns]),]
+  restaurant.data.uniques <- restaurant.data.uniques[,relevant.columns]
+  
+  ## ---- Write CSV file with the table we need to geo locate
+  write_csv(restaurant.data.uniques, '../output/restaurant_uniques_source.csv')
+} else {
+  restaurant.data.uniques <- read_csv('../output/restaurant_uniques_source.csv')
+}
+
+# Function call for geolocation
+get.geocode.data <- function(df, range.to.get.data, fileNumber, source.of.data){
+  df.cut <- df[range.to.get.data,]
+  geocode.data <- geocode(location = df.cut$full.address
+                          ,source = source.of.data
+                          )
+  df.cut <- cbind(df.cut, geocode.data)
+  fileName <- paste0('../output/restaurant_uniques_',fileNumber,'.csv')
+  write_csv(x = df.cut, path = fileName)
+}
 
 
-restaurant.data.uniques <- restaurant.data[!duplicated(restaurant.data[,c('DBA','full.address')]),]
-restaurant.data.uniques <- restaurant.data.uniques[,c('DBA', 'full.address')]
 
 
-restaurant.data.uniques.1 <- restaurant.data.uniques[1:2500,]
 
-restaurant.data.uniques.1 <- restaurant.data.uniques.1 %>%
-  mutate(longtitude = geocode(full.address)[,1],latitude = geocode(full.address)[,2]) # The first var of the geocode function output 
 
-restaurant.data.uniques.2 <- restaurant.data.uniques[2501:2600,]
+# End Preliminary calculations and definitions
 
-restaurant.data.uniques.2 <- restaurant.data.uniques.2 %>%
-  mutate(longtitude = geocode(full.address)[,1],latitude = geocode(full.address)[,2]) # The first var of the geocode function output 
+#
+# KEY PARAMETERS
+#
+source.of.data <- "dsk"
+google.daily.max <- 2500
+fileNumber <- 3
 
-write.csv(restaurant.data2, file="../output/restaurestaurant.data.uniques.2")
+
+# Calculations of range to explore, from a to b
+a <- (fileNumber-1)*google.daily.max + 1
+b <- a + google.daily.max - 1
+# You can change a and b manually in the next two rows
+#a <- 2601
+#b <- 5000
+
+range.to.get.data <- a:b
+
+# Function Call
+get.geocode.data(restaurant.data.uniques,
+                 range.to.get.data,
+                 fileNumber,
+                 source.of.data)
+
+
